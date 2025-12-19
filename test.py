2d63084f7d5301ba98592a83d0c4c54b6972012b
shenglichen-sysu <chenshli23@mail.sysu.edu.cn>
2025-12-19 13:32:09 +0800

    Add files via upload
    A more detailed .py source file will be uploaded soon.

---
+++ test.py
@@ -0,0 +1,11 @@
+
+import PolyDamE
+
+labels = ["Ni","Cr", "Fe", "Nb", "Ti"]
+stoichiometry = [74, 14.9, 7.3, 1., 2.3]
+
+elements = [PolyDamE.get_atomic_data(el) for el in labels]
+E, nu = PolyDamE.solve_polyatomic_damage_energy("InconelX750", \
+                                elements, stoichiometry, E_max=1e7)
+
+
