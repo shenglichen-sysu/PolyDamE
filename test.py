4bb3695a5abf6b6e9e839136a7a216ae9e9b3e3e
shenglichen-sysu <chenshli23@mail.sysu.edu.cn>
2026-05-06 14:52:56 +0800

    Example of the input configuration

--- test.py
+++ test.py
@@ -8,4 +8,5 @@ elements = [PolyDamE.get_atomic_data(el) for el in labels]
 E, nu = PolyDamE.solve_polyatomic_damage_energy("InconelX750", \
                                 elements, stoichiometry, E_max=1e7)
 
-
+print ('================ Calculations finished ================')
+print ('Results are given in the file named {material_name}.dat')
